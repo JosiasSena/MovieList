e32932738843185d29cd4bfb53dd2b542b3931c8
josiassena <josias.sena176@gmail.com>
2018-03-02 20:10:42 -0500

    Added the @parcelize annotation instead

--- core/src/main/java/com/josiassena/core/MovieVideosResult.kt
+++ core/src/main/java/com/josiassena/core/MovieVideosResult.kt
@@ -3,62 +3,47 @@ package com.josiassena.core
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
 /**
  * @author Josias Sena
  */
-@Entity (tableName = "movie_videos_result")
-class MovieVideosResult(@PrimaryKey @SerializedName("id") @Expose var id: String = "",
-                             @ColumnInfo(name = "iso_639_1")
-                             @SerializedName("iso_639_1") @Expose var iso6391: String = "",
-                             @ColumnInfo(name = "iso_3166_1")
-                             @SerializedName("iso_3166_1") @Expose var iso31661: String = "",
-                             @SerializedName("key") @Expose var key: String = "",
-                             @SerializedName("name") @Expose var name: String = "",
-                             @SerializedName("site") @Expose var site: String = "",
-                             @SerializedName("size") @Expose var size: Int = 0,
-                             @SerializedName("type") @Expose var type: String = "") : Parcelable {
+@Parcelize
+@Entity(tableName = "movie_videos_result")
+class MovieVideosResult(@PrimaryKey
+                        @SerializedName("id")
+                        @Expose
+                        var id: String = "",
 
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readInt(),
-            parcel.readString())
+                        @ColumnInfo(name = "iso_639_1")
+                        @SerializedName("iso_639_1")
+                        @Expose
+                        var iso6391: String? = null,
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(iso6391)
-        parcel.writeString(iso31661)
-        parcel.writeString(key)
-        parcel.writeString(name)
-        parcel.writeString(site)
-        parcel.writeInt(size)
-        parcel.writeString(type)
-    }
+                        @ColumnInfo(name = "iso_3166_1")
+                        @SerializedName("iso_3166_1")
+                        @Expose
+                        var iso31661: String? = null,
 
-    override fun describeContents() = 0
+                        @SerializedName("key")
+                        @Expose
+                        var key: String? = null,
 
-    companion object CREATOR : Parcelable.Creator<MovieVideosResult> {
-        override fun createFromParcel(parcel: Parcel) = MovieVideosResult(parcel)
+                        @SerializedName("name")
+                        @Expose
+                        var name: String? = null,
 
-        override fun newArray(size: Int): Array<MovieVideosResult?> = newArray(size)
-    }
+                        @SerializedName("site")
+                        @Expose
+                        var site: String? = null,
 
-    override fun toString(): String {
-        return "MovieVideosResult(id='$id', " +
-                "iso6391='$iso6391', " +
-                "iso31661='$iso31661', " +
-                "key='$key', name='$name', " +
-                "site='$site', " +
-                "size=$size, " +
-                "type='$type')"
-    }
-}
+                        @SerializedName("size")
+                        @Expose
+                        var size: Int? = null,
+
+                        @SerializedName("type")
+                        @Expose
+                        var type: String? = null) : Parcelable
