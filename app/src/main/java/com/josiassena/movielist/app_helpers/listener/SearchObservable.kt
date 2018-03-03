f086565d1c3d87b4ddae7676da8c0ebf142d2038
josiassena <josias.sena176@gmail.com>
2018-03-03 17:39:31 -0500

    SearchObservable clean up

--- app/src/main/java/com/josiassena/movielist/app_helpers/listener/SearchObservable.kt
+++ app/src/main/java/com/josiassena/movielist/app_helpers/listener/SearchObservable.kt
@@ -10,42 +10,33 @@ import io.reactivex.subjects.PublishSubject
 /**
  * @author Josias Sena
  */
-class SearchObservable {
+object SearchObservable : MaterialSearchBar.OnSearchActionListener, TextWatcher {
 
-    companion object {
-        private var subject = PublishSubject.create<String>()
+    private var subject = PublishSubject.create<String>()
 
-        fun fromView(materialSearchBar: MaterialSearchBar): Observable<String> {
-            materialSearchBar
-                    .setOnSearchActionListener(object : MaterialSearchBar.OnSearchActionListener {
-                        override fun onButtonClicked(buttonCode: Int) {
-                        }
+    fun fromView(searchBar: MaterialSearchBar): Observable<String> {
+        searchBar.setOnSearchActionListener(this)
+        searchBar.addTextChangeListener(this)
+        return subject
+    }
 
-                        override fun onSearchStateChanged(stateChanged: Boolean) {
-                            if (!stateChanged) {
-                                subject.onNext("")
-                            }
-                        }
+    override fun onButtonClicked(buttonCode: Int) {
+    }
 
-                        override fun onSearchConfirmed(query: CharSequence?) {
-                            subject.onNext(query.toString())
-                        }
-                    })
-
-            materialSearchBar.addTextChangeListener(object : TextWatcher {
-                override fun afterTextChanged(s: Editable?) {
-                    subject.onNext("$s")
-                }
-
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                }
-            })
-
-            return subject
+    override fun onSearchStateChanged(stateChanged: Boolean) {
+        if (!stateChanged) {
+            subject.onNext("")
         }
+    }
+
+    override fun onSearchConfirmed(query: CharSequence?) = subject.onNext(query.toString())
+
+    override fun afterTextChanged(s: Editable?) = subject.onNext("$s")
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
     }
 
 }
