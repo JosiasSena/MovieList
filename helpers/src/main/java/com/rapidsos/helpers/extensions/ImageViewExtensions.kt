8634ea6c2d63caf787b6fca4f58c250f901f51dd
josiassena <josias.sena176@gmail.com>
2018-03-17 19:23:58 -0400

    Remove unused ImageView extension `getBitmap()`

--- helpers/src/main/java/com/rapidsos/helpers/extensions/ImageViewExtensions.kt
+++ helpers/src/main/java/com/rapidsos/helpers/extensions/ImageViewExtensions.kt
@@ -1,7 +1,5 @@
 package com.rapidsos.helpers.extensions
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.widget.ImageView
 import com.rapidsos.helpers.R
 import com.squareup.picasso.NetworkPolicy
@@ -22,12 +20,4 @@ fun ImageView.setImageFromUrlOffLine(url: String) {
             .placeholder(R.drawable.poster_place_holder)
             .networkPolicy(NetworkPolicy.OFFLINE)
             .into(this)
-}
-
-fun ImageView.getBitmap(): Bitmap? {
-    drawable?.let {
-        return (it as BitmapDrawable).bitmap
-    }
-
-    return null
 }
