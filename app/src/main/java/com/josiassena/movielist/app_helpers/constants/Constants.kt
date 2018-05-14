3185d2e80cc5a49dd95ced0eaac8d6b9b207dc3f
josiassena <josias.sena176@gmail.com>
2018-05-14 00:24:04 -0400

    Added Query types to the constants file

--- app/src/main/java/com/josiassena/movielist/app_helpers/constants/Constants.kt
+++ app/src/main/java/com/josiassena/movielist/app_helpers/constants/Constants.kt
@@ -2,3 +2,11 @@ package com.josiassena.movielist.app_helpers.constants
 
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500"
 const val MOVIE_ID_KEY = "movie_id_key"
+const val QUERY_KEY = "query"
+
+enum class QueryTypes {
+    GENRE,
+    TOP_MOVIES,
+    UPCOMING_MOVIES,
+    NOW_PLAYING
+}
