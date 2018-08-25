32505a6225dfaaf2effa9a7b6e730fd6955f2eeb
josiassena <josias.sena176@gmail.com>
2018-08-25 16:42:59 -0400

    Chrome tabs wrapper around all chrome tab related code.

---
+++ app/src/main/java/com/josiassena/movielist/movie_info/view/ChromeTabLauncher.kt
@@ -0,0 +1,55 @@
+package com.josiassena.movielist.movie_info.view
+
+import android.content.ComponentName
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.customtabs.*
+import android.support.v4.content.ContextCompat
+import com.josiassena.movielist.R
+
+/**
+ * @author Josias Sena
+ */
+class ChromeTabLauncher(private val context: Context) {
+
+    private var customTabsClient: CustomTabsClient? = null
+    private var customTabsSession: CustomTabsSession? = null
+
+    fun init() {
+        CustomTabsClient.bindCustomTabsService(context, "com.android.chrome",
+                object : CustomTabsServiceConnection() {
+
+                    override fun onCustomTabsServiceConnected(name: ComponentName?, client: CustomTabsClient?) {
+                        this@ChromeTabLauncher.customTabsClient = client
+                        this@ChromeTabLauncher.customTabsClient?.warmup(0)
+                        customTabsSession = this@ChromeTabLauncher.customTabsClient?.newSession(null)
+                    }
+
+                    override fun onServiceDisconnected(name: ComponentName?) {
+                    }
+                })
+    }
+
+    fun mayLaunchUrl(firstUrl: String?, otherLikelyUrls: List<String>) {
+        val otherLikelyUrlBundles = otherLikelyUrls.map {
+            val uri = Uri.parse(it)
+            Bundle().apply {
+                putParcelable(CustomTabsService.KEY_URL, uri)
+            }
+        }
+
+        customTabsSession?.mayLaunchUrl(Uri.parse(firstUrl), null, otherLikelyUrlBundles)
+    }
+
+    fun launchUrl(url: String) {
+        val tabsIntent = CustomTabsIntent.Builder()
+                .setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
+                .setStartAnimations(context, R.anim.slide_in_right, R.anim.slide_out_left)
+                .setExitAnimations(context, R.anim.slide_in_left, R.anim.slide_out_right)
+                .build()
+
+        tabsIntent.launchUrl(context, Uri.parse(url))
+    }
+}
