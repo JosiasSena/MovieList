95bc184af7267604b82d9badb35d09ff4c0c5b6a
josiassena <josias.sena176@gmail.com>
2018-11-12 08:51:53 -0500

    Added the new providesNearbyMovieTheatersProvider to the dagger providers class

--- app/src/main/java/com/josiassena/movielist/app_helpers/dependency_injection/modules/ProvidersModule.kt
+++ app/src/main/java/com/josiassena/movielist/app_helpers/dependency_injection/modules/ProvidersModule.kt
@@ -1,10 +1,12 @@
 package com.josiassena.movielist.app_helpers.dependency_injection.modules
 
+import com.josiassena.database.database.DatabaseManager
+import com.josiassena.googleplacesapi.network.GooglePlacesApi
 import com.josiassena.movieapi.Api
 import com.josiassena.movielist.app_helpers.data_providers.genre.GenreProvider
 import com.josiassena.movielist.app_helpers.data_providers.movies.MovieProvider
 import com.josiassena.movielist.app_helpers.data_providers.movies.TopRatedMoviesProvider
-import com.josiassena.database.database.DatabaseManager
+import com.josiassena.movielist.app_helpers.data_providers.theaters_nearby.NearbyMovieTheatersProvider
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -17,21 +19,18 @@ open class ProvidersModule {
 
     @Provides
     @Singleton
-    open fun providesGenreProvider(api: Api, databaseManager: DatabaseManager): GenreProvider {
-        return GenreProvider(api, databaseManager)
-    }
+    open fun providesGenreProvider(api: Api, databaseManager: DatabaseManager) = GenreProvider(api, databaseManager)
 
     @Provides
     @Singleton
-    open fun providesMovieProvider(api: Api, databaseManager: DatabaseManager): MovieProvider {
-        return MovieProvider(api, databaseManager)
-    }
+    open fun providesMovieProvider(api: Api, databaseManager: DatabaseManager) = MovieProvider(api, databaseManager)
 
     @Provides
     @Singleton
-    open fun providesTopRatedMovieProvider(api: Api, databaseManager: DatabaseManager):
-            TopRatedMoviesProvider {
-        return TopRatedMoviesProvider(api, databaseManager)
-    }
+    open fun providesTopRatedMovieProvider(api: Api, databaseManager: DatabaseManager) = TopRatedMoviesProvider(api, databaseManager)
+
+    @Provides
+    @Singleton
+    open fun providesNearbyMovieTheatersProvider(api: GooglePlacesApi) = NearbyMovieTheatersProvider(api)
 
 }
