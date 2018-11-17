94e2af79a804f2b56f993fa8ec01ddbd40cc9df7
josiassena <josias.sena176@gmail.com>
2018-11-16 23:49:13 -0500

    dispose onDestroy

--- app/src/main/java/com/josiassena/movielist/splash/SplashActivity.kt
+++ app/src/main/java/com/josiassena/movielist/splash/SplashActivity.kt
@@ -6,6 +6,7 @@ import com.josiassena.movielist.R
 import com.josiassena.movielist.main.view.MainActivity
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import org.jetbrains.anko.clearTask
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.newTask
@@ -13,15 +14,25 @@ import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var disposable: Disposable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
 
-        Observable.timer(300, TimeUnit.MILLISECONDS)
+        disposable = Observable.timer(300, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
                     startActivity(intentFor<MainActivity>().clearTask().newTask())
                 }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (!disposable.isDisposed) {
+            disposable.dispose()
+        }
     }
 }
