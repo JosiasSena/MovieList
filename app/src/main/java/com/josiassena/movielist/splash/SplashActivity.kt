1990f9a94c3a1a553e51700d9f9add153c4cca83
josiassena <josias.sena176@gmail.com>
2018-11-17 00:11:28 -0500

    Add sample animation to the splash screen using animation sets

--- app/src/main/java/com/josiassena/movielist/splash/SplashActivity.kt
+++ app/src/main/java/com/josiassena/movielist/splash/SplashActivity.kt
@@ -2,37 +2,52 @@ package com.josiassena.movielist.splash
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.josiassena.movielist.R
 import com.josiassena.movielist.main.view.MainActivity
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.activity_splash.*
 import org.jetbrains.anko.clearTask
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.newTask
-import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
-
-    private lateinit var disposable: Disposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
 
-        disposable = Observable.timer(300, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    startActivity(intentFor<MainActivity>().clearTask().newTask())
-                }
+        val appNameAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_appname_animation)
+        appNameAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startLogoAnimation()
+            }
+        })
+
+        tvAppName.startAnimation(appNameAnimation)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    private fun startLogoAnimation() {
+        val logoAnimation = AnimationUtils.loadAnimation(this@SplashActivity, R.anim.splash_logo_animation)
+        logoAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
 
-        if (!disposable.isDisposed) {
-            disposable.dispose()
-        }
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intentFor<MainActivity>().clearTask().newTask())
+            }
+        })
+
+        ivLogo.startAnimation(logoAnimation)
     }
 }
